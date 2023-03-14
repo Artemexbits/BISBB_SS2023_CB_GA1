78d07a2132f0fdb1bba6af6d5293286e8ceebfdf
Artemexbits <=>
2023-03-14 15:53:22 +0100

    added additional info

--- AsciiShape.cs
+++ AsciiShape.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2023 Artemexbits. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 namespace BISBB_SS2023_CB_GA1;
 
 abstract class AsciiShape {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BISBB_SS2023_CB_GA1;
+﻿// Copyright (c) 2023 Artemexbits. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+namespace BISBB_SS2023_CB_GA1;
 class Program
 {
     public static bool isRunning = true;
