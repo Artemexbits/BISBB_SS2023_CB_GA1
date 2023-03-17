819a5e05c55555e996d61a8321c4b807a499aca3
Artemexbits <=>
2023-03-17 15:41:34 +0100

    adapted release.bat

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-copy .\bin\Release\net7.0\win-x64\*.exe .\AsciiGame.exe
+copy .\bin\Release\net7.0\win-x64\publish\BISBB_SS2023_CB_GA1.exe .\AsciiGame.exe
